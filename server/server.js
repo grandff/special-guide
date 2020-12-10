400583537b9e3db27f940f992554b3ebcd7c7f91
김정민 <dsaqaz421@naver.com>
2020-12-10 18:26:54 +0900

    fucking error

--- server/server.js
+++ server/server.js
@@ -1,41 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const app = express()
+const app = express();
 const port = 8080;
-let result;
 
 app.use(cors);
 
+const callData = async() => {
+    let response;
+    try {
+        response = await axios.get('http://apis.data.go.kr/B552555/lhLeaseNoticeInfo/lhLeaseNoticeInfo?serviceKey=yXugn7gIb5dEF5jMX1UjuV4qiy%2Bczf9hX3GZhu%2Fp3kxVI9JbMCvBGPHpKVp5QkAOllYDPlfSnSw0CDX7ZGGYsg%3D%3D&PG_SZ=10&PAGE=1');
+    } catch (e) {
+        console.log("error :: ", e);
+    }
 
-/*
-    ServiceKey : yXugn7gIb5dEF5jMX1UjuV4qiy%2Bczf9hX3GZhu%2Fp3kxVI9JbMCvBGPHpKVp5QkAOllYDPlfSnSw0CDX7ZGGYsg%3D%3D
-    http://apis.data.go.kr/B552555/lhLeaseNoticeInfo
-
-
-    result = await axios.post('https://api.igdb.com/v4/games/', {
-        params : {
-            'Client-ID' : '859alws4abzc7sq461fd4yz8wpi49q',
-            'Authorization' : 'Bearer stc05j4qyhz5hnc4qhi6sjuxy6pmjm'
-        }
-    });  
-
-*/
-
-const getGames = async() => {                           
-    result = await axios.post('http://apis.data.go.kr/B552555/lhLeaseNoticeInfo', {
-        params : {
-            'ServiceKey' : 'yXugn7gIb5dEF5jMX1UjuV4qiy%2Bczf9hX3GZhu%2Fp3kxVI9JbMCvBGPHpKVp5QkAOllYDPlfSnSw0CDX7ZGGYsg%3D%3D'            
-        }
-    });           
-
-    console.log("a ha??", result);
-    app.listen(result, () => console.log(`fucking result ${result}`));
+    return response;
 };
 
+
 app.get("/api", (req, res) => {
-   getGames();
-   res.send(result);
+   callData().then((response) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.json(response.data.response.body);
+   });
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- src/routes/Game.js
+++ src/routes/Game.js
@@ -9,13 +9,28 @@ class Game extends React.Component{
         games : []
     };      
 
-    callGame = async() => {
-        const result = await axios.get('http://localhost:8080/api');
-        console.log("ang ", result);
+    callApi = () => {
+        axios.get("http://localhost:8080/api").then((response) => {
+            console.log(response);
+        });
+    }
+
+    test = async() => {
+        const result = await axios.post("http://apis.data.go.kr/B552555/lhLeaseNoticeInfo/lhLeaseNoticeInfo",
+        {
+            "serviceKey" : "yXugn7gIb5dEF5jMX1UjuV4qiy%2Bczf9hX3GZhu%2Fp3kxVI9JbMCvBGPHpKVp5QkAOllYDPlfSnSw0CDX7ZGGYsg%3D%3D",
+            "PG_SZ" : "10",
+            "PAGE" : "1"
+        }, 
+        {
+            "Access-Control-Allow-Origin" : "*"
+        });
+        console.log(result);
     }
 
     componentDidMount() {        
-        this.callGame();
+        this.callApi();
+        this.test();
     }
 
     render() {
