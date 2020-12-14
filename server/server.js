6a8332039490e426544b96de744011ac59a0f9f9
김정민 <dsaqaz421@naver.com>
2020-12-14 11:33:35 +0900

    fucking mac error

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ app.get("/api", (req, res) => {
    });
 });
 
+app.get("/", (req, res) => {
+    res.send('Hello world!');
+ });
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
